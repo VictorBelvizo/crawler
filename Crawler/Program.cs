d66b0466ff27e37ea905bab9989b2a9a6d53667f
Victor Miranda <victor.miranda@resourceit.com>
2018-04-02 16:42:55 -0300

    Crawler Simples

---
+++ Crawler/Program.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var url = "https://www.google.com.br/search?q=agua&source=lnms&sa=X&ved=0ahUKEwjv6J2sn5zaAhWCGJAKHYGwCn8Q_AUICSgA&biw=1517&bih=447&dpr=0.9";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            //var resultado = doc.GetElementbyId("resultStats").InnerText;
+            var docNode = doc.DocumentNode;
+
+            var node = docNode.SelectNodes("//div[@id = 'main']");
+
+            foreach (var nNode in node.Descendants("a"))
+            {
+                Console.WriteLine(nNode.GetAttributeValue("href", ""));
+            }
+            //Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+    }
+}
